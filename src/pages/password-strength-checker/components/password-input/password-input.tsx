4354ecc188918231d692a0f57940d69722579579
Francis Daniel Valmores <francisdaniel.valmores@engagis.com>
2022-03-28 15:26:47 +0800

    added show/hide toggle for password input

--- src/pages/password-strength-checker/components/password-input/password-input.tsx
+++ src/pages/password-strength-checker/components/password-input/password-input.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
+import classNames from "classnames";
 import "./password-input.scss";
 
 function PasswordInput() {
+  const [visible, setVisible] = useState(false);
+  const [value, setValue] = useState("");
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
   return (
-    <div>
+    <div id="password-input-container">
       <input
         id="password"
         className="text-center"
-        type="password"
+        type={visible ? "text" : "password"}
         placeholder="Type a password"
+        onChange={handleInputChange}
       />
+      <button
+        id="btn-toggle-visibility"
+        className={classNames({ "d-none": !value })}
+        onClick={toggleVisibility}
+      >
+        {visible ? "HIDE" : "SHOW"}
+      </button>
     </div>
   );
 }
